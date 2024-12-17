1af620f05bf1e494b4852627ddf2f30db4ab686d
Mahmoud Abumandour <mam47@sfu.ca>
2024-12-16 18:23:44 -0800

    Use a simpler model for consistency

--- consistency.py
+++ consistency.py
@@ -1,37 +1,21 @@
-import torch
 import re
-import torch.nn.functional as F
-from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer, util
 
 
 class CoherenceScorer:
     def __init__(self, model, max_length):
         self.model = model
         self.max_length = max_length
-        self._init_prefixes()
-
-    def _init_prefixes(self):
-        self._task_name_to_instruct = {
-            "Overall": "Given a question, how well does the passage answer the question?",
-            "Inter-Sentence": "Given two sentences, how well does the second sentence follow the first?",
-        }
-        self.overall_query_prefix = "Instruct" + self._task_name_to_instruct["Overall"] + "\nQuery: "
-        self.inter_sentence_query_prefix = "Instruct" + self._task_name_to_instruct["Inter-Sentence"] + "\nQuery: "
-        self.passage_prefix = ""
 
     def _get_relevance(self, query, passage):
-        qe = self.model.encode([query], instruction=self.query_prefix, max_length=self.max_length)
-        pe = self.model.encode([passage], instruction=self.passage_prefix, max_length=self.max_length)
-
-        qe, pe = F.normalize(qe, p=2, dim=-1), F.normalize(pe, p=2, dim=-1)
-        return torch.mm(qe, pe.T).item()
+        eq = self.model.encode(query, convert_to_tensor=True)
+        ep = self.model.encode(passage, convert_to_tensor=True)
+        return util.pytorch_cos_sim(eq, ep).item()
 
     def _get_inter_sentence_relevalnce(self, sentences):
         res = []
-        self.query_prefix = self.inter_sentence_query_prefix
         for s1, s2 in zip(sentences, sentences[1:]):
             res.append(self._get_relevance(s1, s2))
-
         return sum(res) / len(res)
 
     def _split_sentences(self, text):
@@ -39,14 +23,13 @@ class CoherenceScorer:
 
     def get_coherence_score(self, query, passage):
         sentences = self._split_sentences(passage)
-        self.query_prefix = self.overall_query_prefix
         overall_relevance = self._get_relevance(query, passage)
         inter_sentence_relevance = self._get_inter_sentence_relevalnce(sentences)
-        return overall_relevance * inter_sentence_relevance
+        return overall_relevance * 0.4 + inter_sentence_relevance * 0.6
 
 
 if __name__ == "__main__":
-    model = AutoModel.from_pretrained("nvidia/NV-Embed-v2", trust_remote_code=True)
+    model = SentenceTransformer("all-MiniLM-L6-v2")
     coh = CoherenceScorer(model, 32768)
     query = "What is the meaning of life?"
 
