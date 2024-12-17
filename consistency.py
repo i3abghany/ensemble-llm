24783171683c426442550cb530b8d9f4e104edba
Mahmoud Abumandour <mam47@sfu.ca>
2024-12-16 19:24:30 -0800

    Coherence -> Consistency

--- consistency.py
+++ consistency.py
@@ -2,7 +2,7 @@ import re
 from sentence_transformers import SentenceTransformer, util
 
 
-class CoherenceScorer:
+class ConsistencyScorer:
     def __init__(self, model, max_length):
         self.model = model
         self.max_length = max_length
@@ -21,20 +21,21 @@ class CoherenceScorer:
     def _split_sentences(self, text):
         return re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", text)
 
-    def get_coherence_score(self, query, passage):
+    def evaluate(self, query, passage):
         sentences = self._split_sentences(passage)
         overall_relevance = self._get_relevance(query, passage)
         inter_sentence_relevance = self._get_inter_sentence_relevalnce(sentences)
-        return overall_relevance * 0.4 + inter_sentence_relevance * 0.6
+        return 1 - (overall_relevance * 0.4 + inter_sentence_relevance * 0.6)
 
 
 if __name__ == "__main__":
     model = SentenceTransformer("all-MiniLM-L6-v2")
-    coh = CoherenceScorer(model, 32768)
+    print(type(model))
+    con = ConsistencyScorer(model, 32768)
     query = "What is the meaning of life?"
 
     passage_gpt4 = open("responses/gpt4.txt").read()
-    print("GPT-4:", coh.get_coherence_score(query, passage_gpt4))
+    print("GPT-4:", con.evaluate(query, passage_gpt4))
 
     passage_mistral = open("responses/mistral.txt").read()
-    print("Mistral:", coh.get_coherence_score(query, passage_mistral))
+    print("Mistral:", con.evaluate(query, passage_mistral))
